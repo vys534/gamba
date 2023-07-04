4c8ff44363bd2ba5f573a513b15a913ec1a7edc0
Tyler Thompson <yoitscore@gmail.com>
2023-07-04 02:09:26 +0100

    Unneeded match statements, can be replaced with if let Err

--- src/handler.rs
+++ src/handler.rs
@@ -15,6 +15,7 @@ impl EventHandler for Handler {
         if m.author.bot {
             return;
         }
+
         let mut db_conn = match crate::db::connection::get_sqlite_connection(&self.db).await {
             Ok(c) => c,
             Err(e) => {
@@ -22,29 +23,30 @@ impl EventHandler for Handler {
                 return;
             }
         };
-        match crate::db::on_message::handle(&m.author, self.redis.clone(), &mut db_conn).await {
-            Ok(_) => (),
-            Err(e) => {
-                handle_error(ctx, m, e, false).await;
-                return;
-            }
+
+        if let Err(e) =
+            crate::db::on_message::handle(&m.author, self.redis.clone(), &mut db_conn).await
+        {
+            handle_error(ctx, m, e, false).await;
+            return;
         }
+
         let m_split: Vec<&str> = m.content.split_whitespace().collect();
+
         if m_split.is_empty()
             || !m_split[0].starts_with(crate::PREFIX)
             || m_split[0].len() <= crate::PREFIX.len()
         {
             return;
         }
+
         let cmd_name = &m_split[0][crate::PREFIX.len()..m_split[0].len()];
         let args = crate::util::parse_args(m_split);
         let redis_conn = self.redis.clone();
         for c_test in crate::model::command::command_list().await {
             if cmd_name == c_test.name || cmd_name == c_test.shorthand {
-                let res = c_test.inner.exec(&ctx, &m, db_conn, redis_conn, args).await;
-                match res {
-                    Ok(_) => (),
-                    Err(e) => handle_error(ctx, m, e, true).await,
+                if let Err(e) = c_test.inner.exec(&ctx, &m, db_conn, redis_conn, args).await {
+                    handle_error(ctx, m, e, true).await;
                 }
                 break;
             }
